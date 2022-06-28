6006dbae393328c28f333f70ef7fb40e2f24adf2
SergeiSA1996 <slimmm96@mail.ru>
2022-06-28 17:53:40 +0300

    Start_file

--- hello_django/hello_django/urls.py
+++ hello_django/hello_django/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('home/', views.home),
-    path('reverse', views.reverse)
+    path('reverse/', views.reverse)
 ]
